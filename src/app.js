47c5013ff9a775705fa4cb92f6a25f2fd86f796a
Carlos Fernandez <carlosgustavoforta@gmail.com>
2026-05-23 14:11:29 -0400

    Listo

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ export const app = express();
 
 // Middlewares globales
 app.use(helmet());
-app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
+app.use(cors({ origin: env.CORS_ORIGIN }));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
